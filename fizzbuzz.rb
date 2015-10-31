5bfade5bd33a9e4cbb2bc5dac1da2c74e10dadf0
Kacper Kuś <kacperkus@icloud.com>
2015-10-31 16:41:35 +0100

    Create fizzbuzz.rb

---
+++ fizzbuzz.rb
@@ -0,0 +1,13 @@
+# Fizz Buzz
+
+(1..100).each do |x|
+  if (x % 15 == 0) then
+    puts "FizzBuzz"
+  elsif (x % 3 == 0)
+    puts "Fizz"
+  elsif (x % 5 == 0)
+    puts "Buzz"
+  else
+    puts x
+  end
+end
