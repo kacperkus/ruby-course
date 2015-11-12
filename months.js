cb8bf7d578ff92f52eb62a25b73600a829fea4e3
Kacper Kuś <kacperkus@icloud.com>
2015-11-12 22:39:24 +0100

    Create months.js

---
+++ months.js
@@ -0,0 +1,16 @@
+// Exercise from "Eloquent JavaScript", chapter 10.
+
+var month = function() {
+	var names = ["January", "February", "March", "April",
+                 "May", "June", "July", "August",
+                 "September", "October", "November", "December"];
+  return {
+    name : function(number) { return names[number]; },
+    number : function(name) { return names.indexOf(name); }
+  };
+}();
+
+console.log(month.name(2));
+// → March
+console.log(month.number("November"));
+// → 10
