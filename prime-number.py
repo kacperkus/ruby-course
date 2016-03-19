4eae338d78845cde4cb85953f9fe9554806a3560
Kacper Kuś <kacperkus@icloud.com>
2016-03-19 22:13:27 +0100

    Update prime-number.py

--- prime-number.py
+++ prime-number.py
@@ -1,16 +1,15 @@
 numbers = [2]
+is_prime = False
 
-for number in range(1, 1000):
-    is_prime = False
-    if number > 2:
-        for n in range(2, number):
-            if number % n == 0:
-                is_prime = False
-                break
-            else:
-                is_prime = True
-        if is_prime:
-            numbers.append(number)
+for number in range(3, 1000):
+    for n in range(2, number):
+        if number % n == 0:
+            is_prime = False
+            break
+        else:
+            is_prime = True
+    if is_prime:
+        numbers.append(number)
 
 for number in numbers:
     number = str(number)
